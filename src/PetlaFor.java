b16a55da430df4b6b8edfd5ec441dec69e32b260
patryk <patryknowak9226@gmail.com>
2023-03-16 10:30:36 +0100

    add programu PetlaFor

---
+++ src/PetlaFor.java
@@ -0,0 +1,20 @@
+public class PetlaFor {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        /*for (int i = 0; i <= number; i = i + 2){
+            System.out.println(i);
+        }
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }*/
+
+        for (int j = 0; j <= number; j++)
+            System.out.println(j + " Będę robił prace domową");
+    }
+}
