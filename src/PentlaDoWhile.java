3562eb2a9b5d866c07e5242981b1dcb94067e223
patryk <patryknowak9226@gmail.com>
2023-03-16 10:44:55 +0100

    add programu while i dowhile

---
+++ src/PentlaDoWhile.java
@@ -0,0 +1,10 @@
+public class PentlaDoWhile {
+
+    public static void main(String[] args) {
+        int numbrer = 21;
+        do{
+            System.out.println("moja wartośc to: " + numbrer + " i jest mniejsza od 20");
+            numbrer++;
+        } while (numbrer<20);
+    }
+}
---
+++ src/PentlaWhile.java
@@ -0,0 +1,10 @@
+public class PentlaWhile {
+
+    public static void main(String[] args) {
+        int numbrer = 0;
+        while (numbrer < 20) {
+            System.out.println("moja wartośc to: " + numbrer + " i jest mniejsza od 20");
+            numbrer++;
+            }
+    }
+}
